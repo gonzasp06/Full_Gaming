a466dd538799bc5d48ed56392bae641d943b95f4
gonzasp06 <gonziii_0810@hotmail.com.ar>
2025-12-18 14:54:36 -0300

    Arreglo de falla carrito

--- crear_admin.py
+++ crear_admin.py
@@ -3,10 +3,10 @@ import mysql.connector
 from database import conectar_base_datos
 
 # Datos del admin
-email = ""
-contraseña = ""
-nombre = ""
-apellido = ""
+email = "gonza@admin.com"
+contraseña = "123456789"
+nombre = "Gonzalo"
+apellido = "Sosa"
 
 # Encriptar contraseña
 hashed = bcrypt.hashpw(contraseña.encode('utf-8'), bcrypt.gensalt())
